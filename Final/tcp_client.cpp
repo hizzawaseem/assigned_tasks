fed7edff092d7b9a87d2ab5cf9987a9dc98e2edd
hizzawaseem <55181969+hizzawaseem@users.noreply.github.com>
2021-05-31 02:52:10 +0500

    Update tcp_client.cpp

--- Final/tcp_client.cpp
+++ Final/tcp_client.cpp
@@ -41,15 +41,6 @@ class Client_socket{
             }
             else
                 cout<<"[LOG] : Image file created.\n";
-
-            /*file.open("client_file.txt", ios::out | ios::trunc | ios::binary);
-            if(file.is_open()){
-                cout<<"[LOG] : File Creted.\n";
-            }
-            else{
-                cout<<"[ERROR] : File creation failed, Exititng.\n";
-                exit(EXIT_FAILURE);
-            }*/
         }
 
         void create_socket(){
@@ -69,17 +60,7 @@ class Client_socket{
         }
 
         void receive_image(){
-            /*char buffer[1024] = {};
-            int valread = read(general_socket_descriptor , buffer, 1024);
-            cout<<"[LOG] : Data received "<<valread<<" bytes\n";
-            cout<<"[LOG] : Saving data to file.\n";
-            
-            image<<buffer;
-            cout<<"[LOG] : File Saved.\n";
-            */
-
             int recv_size = 0,size = 0, read_size, write_size, packet_index =1,stat;
-
             char imagearray[27316];
 
             //Find the size of the image
@@ -92,10 +73,6 @@ class Client_socket{
             cout << "[LOG] : Packet size: " << stat << '\n';
             cout << "[LOG] : Image size: " << size << '\n';
 
-            // printf("Packet size: %i\n",stat);
-            // printf("Image size: %i\n",size);
-            // printf(" \n");
-            
             char buffer[27316];
 
             //Send our verification signal
@@ -104,7 +81,6 @@ class Client_socket{
             } while(stat<0);
 
             cout << "[LOG] : Reply sent\n";
-            // printf(" \n");
 
             image = fopen("recieved_image.jpeg", "wb");
 
@@ -121,8 +97,6 @@ class Client_socket{
             int buffer_fd, buffer_out;
 
             while(recv_size < size) {
-            //while(packet_index < 2){
-
                 FD_ZERO(&fds);
                 FD_SET(general_socket_descriptor,&fds);
 
@@ -139,15 +113,11 @@ class Client_socket{
                         read_size = read(general_socket_descriptor,imagearray, 27316);
                     } while(read_size <0);
 
-                    // printf("Packet number received: %i\n",packet_index);
-                    // printf("Packet size: %i\n",read_size);
-
                     cout << "[LOG] : Packet number received: " << packet_index << '\n';
                     cout << "[LOG] : Packet size: " << read_size  << '\n';
                     
                     //Write the currently read data into our image file
                     write_size = fwrite(imagearray,1,read_size, image);
-                    // printf("Written image size: %i\n",write_size); 
                     cout << "[LOG] : Written image size: " << write_size  << '\n';  
 
                     if(read_size !=write_size) {
@@ -157,16 +127,12 @@ class Client_socket{
                     //Increment the total number of bytes read
                     recv_size += read_size;
                     packet_index++;
-                    //printf("Total received image size: %i\n",recv_size);
                     cout << "[LOG] : Total received image size: " << recv_size  << '\n';
-                    // printf(" \n");
-                    // printf(" \n");
                 }
             }
 
             fclose(image);
             cout << "[LOG] : Image successfully Received!\n";
-            // exit(EXIT_FAILURE);
         }
 };
 
